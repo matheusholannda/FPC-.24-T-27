7969ed5ebd3530b2c63c12fa95b8f67eaa6d0e03
matheusholannda <matheushpribeiro@gmail.com>
2024-06-13 19:26:25 -0300

    Upload da aula 13

---
+++ Aula 13 - Funções e métodos/Calculadora/Calculadora/Program.cs
@@ -0,0 +1,99 @@
+﻿Menu();
+
+static void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("Bem-vindo a Calculadora do Holandinha! Digite qual operação deseja executar:\n");
+    Console.WriteLine("1 - Soma");
+    Console.WriteLine("2 - Subtração");
+    Console.WriteLine("3 - Multiplicação");
+    Console.WriteLine("4 - Divisão");
+    Console.WriteLine("5 - Sair da calculadora");
+    Console.Write("\n\n");
+    short res = short.Parse(Console.ReadLine());
+
+    switch (res)
+    {
+        case 1: Soma(); break;
+        case 2: Subtracao(); break;
+        case 3: Multiplicacao(); break;
+        case 4: Divisao(); break;
+        case 5:
+            BarraCarregamento("\nObrigado por usar a calculadora do Holandinha!", 1000, 3);
+            Environment.Exit(0);
+            break;
+        default: Menu(); break;
+    }
+}
+
+static void Soma()
+{
+    Console.Write("\nPrimeiro valor: ");
+    float v1 = float.Parse(Console.ReadLine());
+
+    Console.Write("\n" + v1 + " + ");
+    float v2 = float.Parse(Console.ReadLine());
+
+    float resultado = v1 + v2;
+    Console.Clear();
+    Console.WriteLine("\nO resultado da soma entre: " + v1 + " + " + v2 + " = " + resultado);
+    Console.ReadKey();
+    Menu();
+}
+
+static void Subtracao()
+{
+    Console.Write("\nPrimeiro valor: ");
+    float v1 = float.Parse(Console.ReadLine());
+
+    Console.Write("\n" + v1 + " + ");
+    float v2 = float.Parse(Console.ReadLine());
+
+    float resultado = v1 - v2;
+    Console.Clear();
+    Console.WriteLine("\nO resultado da subtração entre: " + v1 + " - " + v2 + " = " + resultado);
+    Console.ReadKey();
+    Menu();
+}
+static void Multiplicacao()
+{
+    Console.Write("\nPrimeiro valor: ");
+    float v1 = float.Parse(Console.ReadLine());
+
+    Console.Write("\n" + v1 + " + ");
+    float v2 = float.Parse(Console.ReadLine());
+
+    float resultado = v1 * v2;
+    Console.Clear();
+    Console.WriteLine("\nO resultado da multiplicação entre: " + v1 + " * " + v2 + " = " + resultado);
+    Console.ReadKey();
+    Menu();
+}
+static void Divisao()
+{
+    Console.Write("\nPrimeiro valor: ");
+    float v1 = float.Parse(Console.ReadLine());
+
+    Console.Write("\n" + v1 + " + ");
+    float v2 = float.Parse(Console.ReadLine());
+
+    float resultado = v1 / v2;
+    Console.Clear();
+    Console.WriteLine("\nO resultado da divisão entre: " + v1 + " / " + v2 + " = " + resultado);
+    Console.ReadKey();
+    Menu();
+}
+
+static void BarraCarregamento(string texto, int tempo, int quantidade)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+
+    Console.Write(texto);
+
+    for (var contador = 0; contador < quantidade; contador++)
+    {
+        Console.Write(".");
+        Thread.Sleep(tempo);
+    }
+    Console.ResetColor();
+}
---
+++ Aula 13 - Funções e métodos/Exemplo/Exemplo/Program.cs
@@ -0,0 +1,57 @@
+﻿// Fazer um programa para ler três números inteiros e mostrar na tela o maior deles.
+
+/*Sem função
+
+Console.WriteLine("Digite três números");
+int n1 = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+int n3 = int.Parse(Console.ReadLine());
+
+if (n1 > n2 && n2 > n3)
+{
+    Console.WriteLine("Maior = " + n1);
+}
+
+else if (n2 > n3)
+{
+    Console.WriteLine("Maior = " + n2);
+}
+else
+{
+    Console.WriteLine("Maior = " + n3);
+}
+
+Console.ReadKey();*/
+
+//Com função
+
+static void Main(string[] args)
+{
+    Console.WriteLine("Digite três números");
+    int n1 = int.Parse(Console.ReadLine());
+    int n2 = int.Parse(Console.ReadLine());
+    int n3 = int.Parse(Console.ReadLine());
+
+    double resultado = Maior(n1, n2, n3);
+    Console.WriteLine("Maior = " + resultado);
+
+    Console.ReadKey();
+}
+
+static int Maior(int a, int b, int c)
+{
+    int m;
+    if (a > b && a > c)
+    {
+        m = a;
+    }
+    else if (b > c)
+    {
+        m = b;
+    }
+    else
+    {
+        m = c;
+    }
+    return m;
+}
